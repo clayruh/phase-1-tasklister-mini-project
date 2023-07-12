908d61a14bf5752d573c71fae83ccb0b74f840d9
ClaeLu <clarajlu@gmail.com>
2023-07-12 17:20:13 -0400

    create function that adds a new list item on event form submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// As a user, I should be able to type a task into the input field.
+// As a user, I should be able to click some form of a submit button.
+// As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
+
+// first want to grab the form itself
+const createToDo = document.querySelector('#create-task-form')
+
+// then I want to be able to create a new list item with the to-do and append it to the Todos list
+createToDo.addEventListener ("submit", (e) => {
+  e.preventDefault()
+
+  const myToDos = document.querySelector('#list')
+  const newToDo = document.createElement('li')
+
+  console.log(newToDo.textContent = e.target.newTaskDescription.value)
+  myToDos.append(newToDo)
+
+  e.target.reset()
+})
+
+
+// bonus is to create an x-out button
